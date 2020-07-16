fa244410458598b6df26faa35aaac66ea1164214
Joseph Harnish <bigjoe1008@gmail.com>
2020-07-16 11:48:14 -0400

    added dependencies for bootstrap

--- webhandler/webhandler.go
+++ webhandler/webhandler.go
@@ -68,9 +68,12 @@ func (me *Env) ReturnAdminIndex() string {
 }
 
 func ReturnHeader(admin bool) string {
-	output := `<html> 
+	output := `<!doctype html>
+	<html lang="en"> 
 	<head>
 	<title>Battle at the Dawg Pound</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="/style.css">
 	<!-- CSS only -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
