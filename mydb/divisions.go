2d3acaf777e2c215d81f7ed634527f870e52867c
Joseph Harnish <bigjoe1008@gmail.com>
2026-05-10 18:14:05 -0400

    feat: add TournamentID to Division, scope queries to tournament, switch to pgx placeholders

--- mydb/divisions.go
+++ mydb/divisions.go
@@ -1,71 +1,59 @@
 package mydb
 
 import (
+	"database/sql"
 	"log"
-	"strconv"
 )
 
 type Division struct {
-	ID   int
-	Name string
+	ID           int
+	TournamentID int
+	Name         string
 }
 
-func (me *MyDB) AddDivision(name string) {
-
-	query := "Insert into DIVISIONS (DivisionName) values (?);"
-
-	statement, err := me.DB.Prepare(query)
+func (me *MyDB) AddDivision(tournamentID int, name string) {
+	_, err := me.DB.Exec(
+		`INSERT INTO divisions (tournament_id, name) VALUES ($1,$2)`,
+		tournamentID, name,
+	)
 	if err != nil {
-		log.Println("Error - AddDivision - Prepare ", err)
-		return
-	}
-	_, err = statement.Exec(name)
-	if err != nil {
-		log.Println("Error - AddDivison writing ", err)
-		_, err = statement.Exec(name)
+		log.Println("AddDivision:", err)
 	}
 }
 
 func (me *MyDB) DelDivision(id int) {
-	query := "delete from DIVISIONS where id=" + strconv.Itoa(id) + ";"
-	log.Println(query)
-	me.DB.Exec(query)
-
+	me.DB.Exec(`DELETE FROM divisions WHERE id=$1`, id)
 }
 
-func (me *MyDB) ReturnDivisions() (alldivisions []Division) {
-
-	query := "Select id, DivisionName from DIVISIONS;"
-
-	rows, err := me.DB.Query(query)
-	var temp Division
+func (me *MyDB) ReturnDivisions(tournamentID int) []Division {
+	rows, err := me.DB.Query(
+		`SELECT id, tournament_id, name FROM divisions WHERE tournament_id=$1 ORDER BY name`,
+		tournamentID,
+	)
 	if err != nil {
-		log.Println("Error - ReturnDivisons - Query ", err, query)
-		return
+		log.Println("ReturnDivisions:", err)
+		return nil
 	}
+	var out []Division
 	for rows.Next() {
-		rows.Scan(&temp.ID, &temp.Name)
-
-		alldivisions = append(alldivisions, temp)
-
+		var d Division
+		if err := rows.Scan(&d.ID, &d.TournamentID, &d.Name); err != nil {
+			log.Println("ReturnDivisions scan:", err)
+			continue
+		}
+		out = append(out, d)
 	}
 	rows.Close()
-	return
+	return out
 }
 
-func (me *MyDB) ReturnDivisionByID(id int) (division Division) {
-
-	query := "Select id, DivisionName from DIVISIONS where id=" + strconv.Itoa(id) + ";"
-
-	rows, err := me.DB.Query(query)
-	if err != nil {
-		log.Println("Error - ReturnDivisonByID - Query ", err, query)
-		return
+func (me *MyDB) ReturnDivisionByID(id int) Division {
+	var d Division
+	err := me.DB.QueryRow(
+		`SELECT id, tournament_id, name FROM divisions WHERE id=$1`, id,
+	).Scan(&d.ID, &d.TournamentID, &d.Name)
+	if err != nil && err != sql.ErrNoRows {
+		log.Println("ReturnDivisionByID:", err)
 	}
-	for rows.Next() {
-		rows.Scan(&division.ID, &division.Name)
-		rows.Close()
-
-	}
-	return
+	return d
 }
