8eb8795b5cc0ab2bbbe6e02b2493a94d4a65e00d
Joseph Harnish <bigjoe1008@gmail.com>
2026-05-10 18:14:45 -0400

    feat: add TournamentID to Team, scope queries, switch to pgx placeholders

--- mydb/teams.go
+++ mydb/teams.go
@@ -1,192 +1,121 @@
 package mydb
 
 import (
+	"database/sql"
 	"log"
-	"strconv"
 )
 
 type Team struct {
-	ID          int
-	Name        string
-	Coach       string
-	Division    Division
-	Wins        int
-	Losses      int
-	RunsAgainst int
-	RunsFor     int
+	ID           int
+	TournamentID int
+	Name         string
+	Coach        string
+	Division     Division
+	Wins         int
+	Losses       int
+	RunsAgainst  int
+	RunsFor      int
 }
 
-func (me *MyDB) AddTeam(name, coach string, divisionid int) {
-	query := "Insert into TEAMS (teamname, coachname, divisionid) values (?, ?, ?);"
-
-	statement, err := me.DB.Prepare(query)
+func (me *MyDB) AddTeam(tournamentID, divisionID int, name, coach string) {
+	_, err := me.DB.Exec(
+		`INSERT INTO teams (tournament_id, division_id, name, coach) VALUES ($1,$2,$3,$4)`,
+		tournamentID, divisionID, name, coach,
+	)
 	if err != nil {
-		log.Println("Error - AddTeam - Prepare ", err)
-		return
-	}
-	_, err = statement.Exec(name, coach, divisionid)
-	if err != nil {
-		log.Println("Error - AddTeam writing ", err)
-		_, err = statement.Exec(name, coach, divisionid)
+		log.Println("AddTeam:", err)
 	}
 }
 
 func (me *MyDB) DelTeam(id int) {
-
-	me.DB.Exec("delete from TEAMS where id=" + strconv.Itoa(id) + ";")
-
+	me.DB.Exec(`DELETE FROM teams WHERE id=$1`, id)
 }
 
-func (me *MyDB) ReturnTeamsByDivisionID(id int) (teams []Team) {
-
-	query := "Select id, teamname, coachname, divisionid from TEAMS where divisionid=" + strconv.Itoa(id) + ";"
-
-	rows, err := me.DB.Query(query)
-
+func (me *MyDB) ReturnTeamsByDivisionID(divisionID int) []Team {
+	rows, err := me.DB.Query(
+		`SELECT id, tournament_id, name, coach, division_id FROM teams WHERE division_id=$1`,
+		divisionID,
+	)
 	if err != nil {
-		log.Println("Error - ReturnTeamsByDivision - Query ", err, query)
-		return
+		log.Println("ReturnTeamsByDivisionID:", err)
+		return nil
 	}
+	var teams []Team
 	for rows.Next() {
-		var temp Team
-		var divisionid int
-		rows.Scan(&temp.ID, &temp.Name, &temp.Coach, &divisionid)
-		temp.Division = me.ReturnDivisionByID(divisionid)
-		teams = append(teams, temp)
-
+		var t Team
+		var did int
+		if err := rows.Scan(&t.ID, &t.TournamentID, &t.Name, &t.Coach, &did); err != nil {
+			log.Println("ReturnTeamsByDivisionID scan:", err)
+			continue
+		}
+		t.Division = me.ReturnDivisionByID(did)
+		teams = append(teams, t)
 	}
 	rows.Close()
-
-	return
+	return teams
 }
 
-func (me *MyDB) ReturnTeamsByDivisionIDWithStats(id int) (teams []Team) {
-
-	query := "Select id, teamname, coachname, divisionid from TEAMS where divisionid=" + strconv.Itoa(id) + ";"
-
-	rows, err := me.DB.Query(query)
-
-	if err != nil {
-		log.Println("Error - ReturnTeamsByDivision - Query ", err, query)
-		return
+func (me *MyDB) ReturnTeamsByDivisionIDWithStats(divisionID int) []Team {
+	teams := me.ReturnTeamsByDivisionID(divisionID)
+	for i := range teams {
+		teams[i].Wins = me.TeamWins(teams[i].ID)
+		teams[i].Losses = me.TeamLosses(teams[i].ID)
+		teams[i].RunsAgainst = me.TeamScoredAgainst(teams[i].ID)
+		teams[i].RunsFor = me.TeamScoredFor(teams[i].ID)
 	}
-	for rows.Next() {
-		var temp Team
-		var divisionid int
-		rows.Scan(&temp.ID, &temp.Name, &temp.Coach, &divisionid)
-		temp.Division = me.ReturnDivisionByID(divisionid)
-		temp.Wins = me.TeamWins(temp.ID)
-		temp.Losses = me.TeamLosses(temp.ID)
-		temp.RunsAgainst = me.TeamScoredAgainst(temp.ID)
-		temp.RunsFor = me.TeamScoredFor(temp.ID)
-		teams = append(teams, temp)
-
-	}
-	rows.Close()
-
-	return
-}
-
-func (me *MyDB) TeamWins(id int) int {
-	query := "Select count(*) from GAMESBYTEAM where primaryteamid=" + strconv.Itoa(id) + " and teamscore > oppenentscore;"
-
-	rows, err := me.DB.Query(query)
-	if err != nil {
-		log.Println("Error - TeamWins - Query ", err, query)
-		return 0
-	}
-
-	for rows.Next() {
-
-		var wins int
-		rows.Scan(&wins)
-		rows.Close()
-		return wins
-
-	}
-	return 0
-}
-
-func (me *MyDB) TeamLosses(id int) int {
-	query := "Select count(*) from GAMESBYTEAM where primaryteamid=" + strconv.Itoa(id) + " and teamscore < oppenentscore;"
-
-	rows, err := me.DB.Query(query)
-	if err != nil {
-		log.Println("Error - TeamLosses - Query ", err, query)
-		return 0
-	}
-
-	for rows.Next() {
-
-		var losses int
-		rows.Scan(&losses)
-		rows.Close()
-		return losses
-
-	}
-	return 0
+	return teams
 }
 
 func (me *MyDB) ReturnTeamByID(id int) Team {
-
-	query := "Select id, teamname, coachname, divisionid from TEAMS where id=" + strconv.Itoa(id) + ";"
-
-	rows, err := me.DB.Query(query)
-	var temp Team
-	if err != nil {
-		log.Println("Error - ReturnTeamByID - Query ", err, query)
-		return temp
+	var t Team
+	var did int
+	err := me.DB.QueryRow(
+		`SELECT id, tournament_id, name, coach, division_id FROM teams WHERE id=$1`, id,
+	).Scan(&t.ID, &t.TournamentID, &t.Name, &t.Coach, &did)
+	if err != nil && err != sql.ErrNoRows {
+		log.Println("ReturnTeamByID:", err)
+		return t
 	}
-
-	for rows.Next() {
-
-		var divisionid int
-		rows.Scan(&temp.ID, &temp.Name, &temp.Coach, &divisionid)
-		temp.Division = me.ReturnDivisionByID(divisionid)
-		rows.Close()
-		return temp
-
-	}
-
-	return temp
+	t.Division = me.ReturnDivisionByID(did)
+	return t
 }
 
-func (me *MyDB) TeamScoredAgainst(id int) int {
-	query := "Select sum(oppenentscore) from GAMESBYTEAM where primaryteamid=" + strconv.Itoa(id) + ";"
+func (me *MyDB) TeamWins(id int) int {
+	var n int
+	me.DB.QueryRow(
+		`SELECT COUNT(*) FROM games_by_team WHERE team_id=$1 AND team_score > opponent_score`, id,
+	).Scan(&n)
+	return n
+}
 
-	rows, err := me.DB.Query(query)
-	if err != nil {
-		log.Println("Error - TeamScoredAgainst - Query ", err, query)
-		return 0
-	}
-
-	for rows.Next() {
-
-		var score int
-		rows.Scan(&score)
-		rows.Close()
-		return score
-
-	}
-	return 0
+func (me *MyDB) TeamLosses(id int) int {
+	var n int
+	me.DB.QueryRow(
+		`SELECT COUNT(*) FROM games_by_team WHERE team_id=$1 AND team_score < opponent_score`, id,
+	).Scan(&n)
+	return n
 }
 
 func (me *MyDB) TeamScoredFor(id int) int {
-	query := "Select sum(teamscore) from GAMESBYTEAM where primaryteamid=" + strconv.Itoa(id) + ";"
-
-	rows, err := me.DB.Query(query)
-	if err != nil {
-		log.Println("Error - TeamScoredFor - Query ", err, query)
+	var s sql.NullInt64
+	me.DB.QueryRow(`SELECT SUM(team_score) FROM games_by_team WHERE team_id=$1`, id).Scan(&s)
+	if !s.Valid {
 		return 0
 	}
+	return int(s.Int64)
+}
 
-	for rows.Next() {
-
-		var score int
-		rows.Scan(&score)
-		rows.Close()
-		return score
-
+func (me *MyDB) TeamScoredAgainst(id int) int {
+	var s sql.NullInt64
+	me.DB.QueryRow(`SELECT SUM(opponent_score) FROM games_by_team WHERE team_id=$1`, id).Scan(&s)
+	if !s.Valid {
+		return 0
 	}
-	return 0
+	return int(s.Int64)
+}
+
+func (me *MyDB) GamesPlayedByTeam(id int) int {
+	var n int
+	me.DB.QueryRow(`SELECT COUNT(*) FROM games_by_team WHERE team_id=$1`, id).Scan(&n)
+	return n
 }
